e4edb92476a1973610d838019a3cbd17739f5c28
Gutenberg <147880805+GutenbergJ@users.noreply.github.com>
2024-01-04 01:23:02 +0300

    Add a function that plays one round of RPS

--- javascript.js
+++ javascript.js
@@ -3,10 +3,36 @@ function getComputerChoice() {
     let usableRandomNumber = +randomNumber.slice(2, 4);
 
     if (usableRandomNumber >= 0 && usableRandomNumber <= 33) {
-        return "Rock";
+        return "rock";
     } else if (usableRandomNumber >= 34 && usableRandomNumber <= 66) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+playerSelection = playerSelection.toLowerCase();
+if (playerSelection === "rock" && computerSelection === "rock") {
+    return "Tie! Rock is equal to rock.";
+} else if (playerSelection === "paper" && computerSelection === "paper") {
+    return "Tie! Paper is equal to paper.";
+} else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    return "Tie! Scissors are equal to scissors.";
+} else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    return "Tie! Scissors are equal to scissors.";
+} else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats scissors.";
+} else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats rock.";
+} else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beat paper.";
+} else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper beats rock.";
+} else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose! Scissors beat paper.";
+} else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose! Rock beats scissors.";
+}}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(); 
